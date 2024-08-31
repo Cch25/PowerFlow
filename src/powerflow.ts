1e53f52a59b3a77c6746d1c23b9da8603199ee3c
Culai Chiritoiu <c.chiritoiu07@gmail.com>
2024-08-31 22:07:35 +0300

    Some nice ui

--- src/powerflow.ts
+++ src/powerflow.ts
@@ -2,19 +2,30 @@ import { PfMath } from "./math/utils";
 import { Point } from "./primitives/point";
 import { ViewPort } from "./viewport";
 
+type PFObject = {
+  label: string;
+  pos: Point;
+};
+
 export class PowerFlow {
   private readonly context: CanvasRenderingContext2D;
-  private foundSq: Point | null = null;
+  private foundPfObj: PFObject | null = null;
 
-  private squares: Point[] = [Point.new(50, 50), Point.new(-200, -200)];
+  private pfObjects: PFObject[] = [
+    { label: "A", pos: Point.new(50, 50) },
+    { label: "B", pos: Point.new(-200, -200) },
+    { label: "C", pos: Point.new(-150, -50) }
+  ];
 
   constructor(private readonly viewPort: ViewPort) {
     this.context = this.viewPort.context;
 
     this.viewPort.canvas.addEventListener("mousemove", (e) => {
-      for (const square of this.squares) {
-        if (PfMath.distanceTo(square, this.viewPort.getMouse(e)) < 50) {
-          this.foundSq = square;
+      for (const square of this.pfObjects) {
+        if (
+          PfMath.distanceTo(square.pos, this.viewPort.getMouse(e, true)) < 50
+        ) {
+          this.foundPfObj = square;
           break;
         }
       }
@@ -22,35 +33,40 @@ export class PowerFlow {
   }
 
   public draw() {
-    for (const square of this.squares) {
-      this.drawSquare(square.x, square.x);
+    for (const pfObj of this.pfObjects) {
+      this.drawCircle(pfObj);
     }
 
-    if (this.foundSq) {
-      this.drawText(this.foundSq);
+    if (this.foundPfObj) {
+      this.drawText(this.foundPfObj);
     }
   }
 
-  private drawText(point: Point) {
-    this.context.fillStyle = "red";
-    this.context.font = "32px serif";
+  private drawText(obj: PFObject) {
+    this.context.fillStyle = "#aa12aa";
+    this.context.font = "19px serif";
     this.context.fillText(
-      `Touched square: ${point.x}, ${point.y}`,
-      point.x,
-      point.y
+      `Touched square: ${obj.label} ${obj.pos.x}, ${obj.pos.y}`,
+      obj.pos.x + 50,
+      obj.pos.y 
     );
   }
 
-  private drawSquare(x: number, y: number) {
-    this.context.beginPath();
+  private drawCircle(pfObj: PFObject) {
     this.context.save();
-    this.context.translate(x, y);
-    this.context.moveTo(0, 0);
-    this.context.lineTo(0, -50);
-    this.context.lineTo(50, -50);
-    this.context.lineTo(50, 0);
-    this.context.lineTo(0, 0);
-    this.context.stroke();
+    this.context.beginPath();
+    this.context.translate(pfObj.pos.x / 2, pfObj.pos.y / 2);
+    this.context.arc(pfObj.pos.x / 2, pfObj.pos.y / 2, 50, 0, 2 * Math.PI);
+    this.context.fillStyle = "#cc22c1";
+    this.context.strokeStyle = "#a2a2a2";
+    this.context.fill();
+    this.context.closePath();
+
+    this.context.fillStyle = "#f3f2f1";
+    this.context.font = "18px serif";
+    this.context.textAlign = "center";
+    this.context.fillText(`${pfObj.label}`, pfObj.pos.x / 2, pfObj.pos.y / 2);
+
     this.context.restore();
   }
 }
